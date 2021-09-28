4e5f867c29577539c2dd6bf4f077a14a4a84975f
이용재 <dev.yongjaelee@gmail.com>
2021-09-28 22:04:51 +0900

    env: React를 사용할 수 있도록 Webpack 및 Babel 설정을 한다
    * base config를 dev, prod 에서 merge하여 사용하게끔 함
    * 보편적인 바벨 설정
    * test 페이지가 렌더링 가능하도록 구성

---
+++ webpack.base.js
@@ -0,0 +1,38 @@
+import path from 'path';
+
+const __dirname = path.resolve();
+
+const PROJECT_ROOT = path.resolve(__dirname, './');
+const DIST_PATH = path.resolve(PROJECT_ROOT, 'dist');
+const SRC_PATH = path.resolve(PROJECT_ROOT, 'src');
+
+export const TEMPLATE_PATH = path.resolve(PROJECT_ROOT, 'public');
+
+export default {
+  entry: path.resolve(SRC_PATH, 'index.tsx'),
+  output: {
+    path: DIST_PATH,
+    filename: '[name].js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        include: path.resolve('src'),
+
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    alias: {
+      '@src': SRC_PATH,
+    },
+    modules: ['node_modules'],
+  },
+};
