752f6f13f7826caaf30e8fe334be90e68b334d57
dididy <dev.yongjaelee@gmail.com>
2021-10-02 00:35:23 +0900

    fix: test-utils에 ThemeProvider를 연결한다

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,14 +1,27 @@
 // code reference: https://redux.js.org/usage/writing-tests#components
-import React from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@emotion/react';
+import { themeLight, themeDark } from '@src/styles/theme';
 
 // Import your own modules
-import store from '@src/store';
+import store, { useUiState } from '@src/store';
 
-const render = (component: React.ReactElement) => {
-  const Wrapper = ({ children }: { children: React.ReactNode }) => (
-    <Provider store={store}>{children}</Provider>
+const Root = ({ children }: { children: ReactNode }): ReactElement => {
+  const { isDark } = useUiState();
+
+  return (
+    <ThemeProvider theme={isDark ? themeDark : themeLight}>
+      {children}
+    </ThemeProvider>
+  );
+};
+const render = (component: ReactElement) => {
+  const Wrapper = ({ children }: { children: ReactNode }) => (
+    <Provider store={store}>
+      <Root>{children}</Root>
+    </Provider>
   );
   return rtlRender(component, { wrapper: Wrapper });
 };
