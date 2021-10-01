3c953ebdd7ac54ee4653c60d9f60bced0df6a564
dididy <dev.yongjaelee@gmail.com>
2021-10-01 23:25:57 +0900

    env: emotion에 normalize css 를 추가한다

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React, { ReactElement, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@emotion/react';
+import { ThemeProvider, Global } from '@emotion/react';
 
 import App from '@src/App';
 import { themeLight, themeDark } from '@src/styles/theme';
+import globalStyles from '@src/styles/globalStyles';
 
 const Root = (): ReactElement => {
   const [isDark, setIsDark] = useState<boolean>(false);
 
   return (
     <ThemeProvider theme={isDark ? themeDark : themeLight}>
+      <Global styles={globalStyles} />
       <App isDark={isDark} setIsDark={setIsDark} />
     </ThemeProvider>
   );
