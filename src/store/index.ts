c7fb6b86443b9bb854f62a75ca81f5c392f9b29d
kand193 <qkrdudwls99@gmail.com>
2021-10-02 21:37:06 +0900

    fix: Diary 타입 정의 및 기본 Store 생성

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,11 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { useSelector, useDispatch } from 'react-redux';
 
 import uiReducer, { setLoading, setIsDark } from '@src/store/uiSlice';
+import diaryReducer from '@src/store/diarySlice';
 
 const reducers = combineReducers({
   ui: uiReducer,
+  diary: diaryReducer,
 });
 
 const store = configureStore({ reducer: reducers });
@@ -19,3 +21,4 @@ export type RootState = ReturnType<typeof store.getState>;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 export const useUiState = () => useSelector<RootState, RootState['ui']>((state) => state.ui);
+export const useDiaryState = () => useSelector<RootState, RootState['diary']>((state) => state.diary);
