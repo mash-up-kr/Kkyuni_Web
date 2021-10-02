7f7f8cbea754a3695572ef98b6be37a764d34a35
dididy <dev.yongjaelee@gmail.com>
2021-10-02 21:18:07 +0900

    env: @svgr/webpack loader를 추가한다

--- .storybook/webpack.config.cjs
+++ .storybook/webpack.config.cjs
@@ -4,9 +4,18 @@ const PROJECT_ROOT = path.resolve(__dirname, '../.');
 const SRC_PATH = path.resolve(PROJECT_ROOT, 'src');
 
 module.exports = async ({ config }) => {
+  config.module.rules.push(   {
+    test: /\.svg$/,
+    use: [{
+      loader: '@svgr/webpack',
+    }],
+  });
+
+  
   config.resolve.alias = {
     '@src': SRC_PATH,
   };
 
+  // don't forget to return.
   return config;
 };
