ce02ba4476dd10549fda1a744838eb800c904c1b
dididy <dev.yongjaelee@gmail.com>
2021-10-16 17:45:51 +0900

    fix: 실수로 저장하지 않은 custom-type 변경점을 반영한다

--- @types/custom-types/index.d.ts
+++ @types/custom-types/index.d.ts
@@ -3,33 +3,4 @@ declare module 'custom-types' {
   namespace customTypes {
     type State<T> = Dispatch<SetStateAction<T>>;
   }
-
-  namespace diary {
-    type YoutubeInfo = {
-      link: string;
-      title: string;
-      thumbnail: string;
-      playTime: number;
-    }
-
-    type Emotion =
-      | 'ANGRY'
-      | 'FUNNY'
-      | 'NOTBAD'
-      | 'PANIC'
-      | 'SAD';
-
-    type DiaryType =
-      | 'BLUE'
-      | 'GREEN';
-
-    type Diary = {
-      type: DiaryType;
-      date: Date;
-      title: string;
-      content: string;
-      youtubeInfo: YoutubeInfo;
-      emotion: Emotion;
-    }
-  }
 }
