be2389503a99c94086d95c2f4061e6b6ba181ed0
dididy <dev.yongjaelee@gmail.com>
2021-10-16 14:25:31 +0900

    refactor: type declare 관련하여 정리한다
    * declaner, namespace는 export가 필요 없음
    * extension-type으로 파일명 변경

--- @types/custom-types/index.d.ts
+++ @types/custom-types/index.d.ts
@@ -1,6 +1,35 @@
 declare module 'custom-types' {
   // eslint-disable-next-line import/prefer-default-export
-  export namespace customTypes {
+  namespace customTypes {
     type State<T> = Dispatch<SetStateAction<T>>;
+  }
+
+  namespace diary {
+    type YoutubeInfo = {
+      link: string;
+      title: string;
+      thumbnail: string;
+      playTime: number;
+    }
+
+    type Emotion =
+      | 'ANGRY'
+      | 'FUNNY'
+      | 'NOTBAD'
+      | 'PANIC'
+      | 'SAD';
+
+    type DiaryType =
+      | 'BLUE'
+      | 'GREEN';
+
+    type Diary = {
+      type: DiaryType;
+      date: Date;
+      title: string;
+      content: string;
+      youtubeInfo: YoutubeInfo;
+      emotion: Emotion;
+    }
   }
 }
