aa5d6c9038822bb11c3d96e2ace7cee89f9f8635
kand193 <qkrdudwls99@gmail.com>
2021-11-02 22:54:06 +0900

    feat: Add kakao button

---
+++ src/components/KakaoButton/index.tsx
@@ -0,0 +1,47 @@
+import React, { FC, useCallback, useEffect } from 'react';
+import styled from '@emotion/styled';
+
+declare const Kakao: any;
+
+interface KakaoButtonProps {
+  url: string;
+}
+
+const KakaoButton: FC<KakaoButtonProps> = ({ url }: KakaoButtonProps) => {
+  useEffect(() => {
+    if (!Kakao.isInitialized()) {
+      Kakao.init('5fb43f5a2ce5219ff1ded0325d9d91e7');
+    }
+  }, []);
+
+  const onClick = useCallback(() => {
+    Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: '뀨니뀨니',
+        description: '공유할끄니까',
+        imageUrl:
+          'https://k.kakaocdn.net/14/dn/btrjLxLjDDM/rIU0DYblVkzQRKl7SaiKN1/o.jpg',
+        link: {
+          mobileWebUrl: url,
+          webUrl: url,
+        },
+      },
+    });
+  }, []);
+
+  return (
+    <PointerImage
+      src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png"
+      onClick={onClick}
+    />
+  );
+};
+
+export default KakaoButton;
+
+const PointerImage = styled.img`
+  width: 60px;
+  height: 60px;
+  cursor: pointer;
+`;
