dcf3e84feb1899eb5499fc074716f93ef1f4de33
kand193 <qkrdudwls99@gmail.com>
2021-12-09 10:25:09 +0900

    fix: Remove nullish coalescing operator

--- src/api/diaryApi.ts
+++ src/api/diaryApi.ts
@@ -24,7 +24,7 @@ export type GetDiaryResponseType = {
 export const convertResponseToDiary = (newDiary: GetDiaryResponseType): Diary => ({
   emotion: newDiary.emotion as Emotion,
   title: newDiary.title,
-  type: (newDiary.diaryType as DiaryType) ?? 'BLUE1',
+  type: (newDiary.diaryType as DiaryType) || 'BLUE1',
   date: new Date(newDiary.writingDate),
   content: newDiary.content,
   youtubeInfo: {
